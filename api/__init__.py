90a78db676ea76599baa71aa01eaba9fa47c4d2b
Carlos A. Rodriguez <charlie.mojocoa@gmail.com>
2021-07-11 16:33:38 -0400

    add placeholder endpoints

--- api/__init__.py
+++ api/__init__.py
@@ -1,10 +1,26 @@
 from flask import Flask
+from flask_restful import Api, Resource
 
 
 app = Flask(__name__)
+api = Api(app)
 
 
-@app.route("/")
-def index():
-    """ home page """
-    return "Hello World!"
+class Record(Resource):
+    def delete(self, record_id):
+        return {"method": "DELETE"}
+
+    def get(self, record_id):
+        return {"method": "GET"}
+
+    def put(self, record_id):
+        return {"method": "PUT"}
+
+
+class NewRecord(Resource):
+    def post(self):
+        return {"method": "POST"}
+
+
+api.add_resource(Record, "/record/<int:record_id>")
+api.add_resource(NewRecord, "/record")
