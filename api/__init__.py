edc047b871c195a05bc6f15006f0b3282ae0cd50
Carlos A. Rodriguez <charlie.mojocoa@gmail.com>
2021-07-12 22:07:54 -0400

    add NewRecordResource POST logic

--- api/__init__.py
+++ api/__init__.py
@@ -1,10 +1,11 @@
 import os
 
 from dotenv import load_dotenv
-from flask import Flask
+from flask import Flask, request
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 from flask_sqlalchemy import SQLAlchemy
+from marshmallow import fields, Schema, validates, ValidationError
 from sqlalchemy import CheckConstraint, MetaData
 
 
@@ -47,9 +48,34 @@ class Record(db.Model):
         CheckConstraint("humidity >= 0 AND humidity <= 100", name="humidity")
     )
 
+# SCHEMAS
+
+class RecordSchema(Schema):
+    record_id = fields.Int(dump_only=True)
+    timestamp = fields.Float(required=True)
+    temperature = fields.Float(required=True)
+    humidity = fields.Float(required=True)
+
+    @validates("temperature")
+    def validate_temperature(self, value):
+        if not (-100 <= value <= 100):
+            raise ValidationError(
+                "temperature must be between -100 and 100 degrees Celcius (inclusive)"
+            )
+
+    @validates("humidity")
+    def validate_humidity(self, value):
+        if not (0 <= value <= 100):
+            raise ValidationError(
+                "humidity must be between 0 and 100 percent (inclusive)"
+            )
+
+record_schema = RecordSchema()
+records_schema = RecordSchema(many=True)
+
 # API
 
-class RecordEndPoint(Resource):
+class RecordResource(Resource):
     def delete(self, record_id):
         return {"method": "DELETE"}
 
@@ -60,10 +86,29 @@ class RecordEndPoint(Resource):
         return {"method": "PUT"}
 
 
-class NewRecordEndPoint(Resource):
+class NewRecordResource(Resource):
     def post(self):
-        return {"method": "POST"}
+        body = request.get_json()
+
+        try:
+            data = record_schema.load(body)
+        except ValidationError as e:
+            return {"record": {}, "errors": e.messages}, 422
+
+        record = Record(**data)
+
+        db.session.add(record)
+        db.session.commit()
+        db.session.refresh(record)
+
+        return (
+            {
+                "record": record_schema.dump(record), 
+                "errors": []
+            },
+            201
+        )    
 
 
-api.add_resource(RecordEndPoint, "/record/<int:record_id>")
-api.add_resource(NewRecordEndPoint, "/record")
+api.add_resource(RecordResource, "/record/<int:record_id>")
+api.add_resource(NewRecordResource, "/record")
